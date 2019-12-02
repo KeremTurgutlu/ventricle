ecfdebea67e789f6a8ece97eae77ce7913d102fd
Kerem Turgutlu <keremturgutlu@gmail.com>
2019-12-01 18:18:52 -0800

    reogarnize

--- dev/init.sh
+++
@@ -1,3 +0,0 @@
-# Set yaml config file absolute path here
-# source init.sh
-export YAML_PATH=/home/turgutluk/Vent_Seg_Project/dev/data.yaml
--- dev/normalize.sh
+++
@@ -1,6 +0,0 @@
-source ./init.sh
-mkdir -p .log
-
-# python local/normalize.py --output_path=ATLAS  2>&1 | tee .log/normalize.log 
-python local/normalize.py --output_path=MR  2>&1 | tee .log/normalize.log 
-# python local/normalize.py --output_path=CT  2>&1 | tee .log/normalize.log 
